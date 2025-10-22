856efd60baa979b84f98d31d61caa35171bf020a
will engleby <wengleby@ufl.edu>
2025-10-22 20:02:47 +0900

    added walls and textures to test level

--- testingenvironments/wills3dtest/addons/proto_controller/proto_controller.gd
+++ testingenvironments/wills3dtest/addons/proto_controller/proto_controller.gd
@@ -1,59 +1,44 @@
 # ProtoController v1.0 by Brackeys
 # CC0 License
-# Intended for rapid prototyping of first-person games.
-# Happy prototyping!
 
 extends CharacterBody3D
+
 @onready var camera_main : Camera3D = $Head/Camera3D
 @onready var camera_side : Camera3D = $Head/Camera3D2
-## Can we move around?
+@onready var mesh_3d : MeshInstance3D = $MeshInstance3D           # Path to your 3D mesh
+@onready var mesh_2d : MeshInstance2D = $MeshInstance2D           # Path to your 2D mesh
+
 @export var can_move : bool = true
-## Are we affected by gravity?
 @export var has_gravity : bool = true
-## Can we press to jump?
 @export var can_jump : bool = true
-## Can we hold to run?
 @export var can_sprint : bool = false
-## Can we press to enter freefly mode (noclip)?
 @export var can_freefly : bool = false
 
 @export_group("Speeds")
-## Look around rotation speed.
 @export var look_speed : float = 0.002
-## Normal speed.
 @export var base_speed : float = 7.0
-## Speed of jump.
 @export var jump_velocity : float = 4.5
-## How fast do we run?
 @export var sprint_speed : float = 10.0
-## How fast do we freefly?
 @export var freefly_speed : float = 25.0
 
 @export_group("Input Actions")
-## Name of Input Action to move Left.
 @export var input_left : String = "ui_left"
-## Name of Input Action to move Right.
 @export var input_right : String = "ui_right"
-## Name of Input Action to move Forward.
 @export var input_forward : String = "ui_up"
-## Name of Input Action to move Backward.
 @export var input_back : String = "ui_down"
-## Name of Input Action to Jump.
 @export var input_jump : String = "ui_accept"
-## Name of Input Action to Sprint.
 @export var input_sprint : String = "sprint"
-## Name of Input Action to toggle freefly mode.
 @export var input_freefly : String = "freefly"
 
 var mouse_captured : bool = false
 var look_rotation : Vector2
 var move_speed : float = 0.0
 var freeflying : bool = false
-
-# Added: Slice toggle variable
 var is_sliced : bool = false
 
-## IMPORTANT REFERENCES
+var default_collision_size := Vector3(0.5, 1.8, 0.5)
+var thin_collision_size := Vector3(0.02, 1.8, 0.5) # Adjust as needed
+
 @onready var head: Node3D = $Head
 @onready var collider: CollisionShape3D = $Collider
 
@@ -61,37 +46,51 @@ func _ready() -> void:
 	check_input_mappings()
 	look_rotation.y = rotation.y
 	look_rotation.x = head.rotation.x
+	# Set visibility for models
+	if mesh_3d:
+		mesh_3d.visible = true
+	if mesh_2d:
+		mesh_2d.visible = false
 
 func _unhandled_input(event: InputEvent) -> void:
-	# Mouse capturing
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		capture_mouse()
-	if Input.is_key_pressed(KEY_ESCAPE):
-		release_mouse()
-
-	# Look around
-	if mouse_captured and event is InputEventMouseMotion:
+	if not is_sliced:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			capture_mouse()
+		if Input.is_key_pressed(KEY_ESCAPE):
+			release_mouse()
+	if mouse_captured and event is InputEventMouseMotion and not is_sliced:
 		rotate_look(event.relative)
 
-	# Toggle freefly mode
 	if can_freefly and Input.is_action_just_pressed(input_freefly):
 		if not freeflying:
 			enable_freefly()
 		else:
 			disable_freefly()
-	
-	# Fixed: Toggle slice mode with "g" using event.keycode (Godot 4.x)
+
 	if event is InputEventKey and event.keycode == KEY_G and event.pressed and not event.echo:
 		is_sliced = !is_sliced
 		if is_sliced:
 			camera_side.set_current(true)
 			camera_main.set_current(false)
+			if mesh_3d:
+				mesh_3d.visible = false
+			if mesh_2d:
+				mesh_2d.visible = true
+			if collider.shape is BoxShape3D:
+				collider.shape.size = thin_collision_size
+			release_mouse()
+			mouse_captured = false
 		else:
 			camera_main.set_current(true)
 			camera_side.set_current(false)
+			if mesh_3d:
+				mesh_3d.visible = true
+			if mesh_2d:
+				mesh_2d.visible = false
+			if collider.shape is BoxShape3D:
+				collider.shape.size = default_collision_size
 
 func _physics_process(delta: float) -> void:
-	# If freeflying, handle freefly and nothing else
 	if can_freefly and freeflying:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
 		var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -99,26 +98,21 @@ func _physics_process(delta: float) -> void:
 		move_and_collide(motion)
 		return
 
-	# Apply gravity to velocity
 	if has_gravity:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 
-	# Apply jumping
 	if can_jump:
 		if Input.is_action_just_pressed(input_jump) and is_on_floor():
 			velocity.y = jump_velocity
 
-	# Modify speed based on sprinting
 	if can_sprint and Input.is_action_pressed(input_sprint):
 		move_speed = sprint_speed
 	else:
 		move_speed = base_speed
 
-	# Apply desired movement to velocity
 	if can_move:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
-		# Only allow forward/backward movement if sliced mode is on
 		if is_sliced:
 			input_dir.x = 0
 		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -132,17 +126,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 		velocity.y = 0
 
-	# Use velocity to actually move
 	move_and_slide()
 	if is_sliced:
 		var side_offset = Vector3(5, 2, 0)
 		camera_side.global_position = global_position + (global_transform.basis.x * 5) + Vector3.UP * 2
 		camera_side.look_at(global_position, Vector3.UP)
 
-## Rotate us to look around.
-## Base of controller rotates around y (left/right). Head rotates around x (up/down).
-## Modifies look_rotation based on rot_input, then resets basis and rotates by look_rotation.
 func rotate_look(rot_input : Vector2):
+	if is_sliced:
+		return
 	look_rotation.x -= rot_input.y * look_speed
 	look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
 	look_rotation.y -= rot_input.x * look_speed
@@ -168,8 +160,6 @@ func release_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	mouse_captured = false
 
-## Checks if some Input Actions haven't been created.
-## Disables functionality accordingly.
 func check_input_mappings():
 	if can_move and not InputMap.has_action(input_left):
 		push_error("Movement disabled. No InputAction found for input_left: " + input_left)
