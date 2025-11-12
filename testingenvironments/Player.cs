aabac07bc19be70d959a58da9679444dad871441
cainan medeiros <cainanmedeiros@live.com>
2025-11-12 15:24:37 +0900

    Rewriting 2D player controller to have states: falling & idle states implemented

--- testingenvironments/Player.cs
+++ testingenvironments/Player.cs
@@ -1,45 +1,46 @@
+using System.ComponentModel;
 using Godot;
+
 
 public partial class Player : CharacterBody2D
 {
 	[Export] public float MoveSpeed = 220f;
 	[Export] public float JumpVelocity = 350f;
-	[Export] public float _gravity = 900f;
+	[Export] public float _gravity = 2000f;
+	
+	// Possible States
+	enum State { Idle, Run }
+	private State current_state;
 
-	public override void _Ready()
+    
+
+    public override void _Ready()
 	{
-		_gravity = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
-		GD.Print("Player _Ready OK");
+		current_state = State.Idle;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		// Prove _PhysicsProcess runs
-		// (Comment this after confirming to avoid spam)
-		// GD.Print("tick");
+		player_falling(delta);
+		player_idle(delta);
 
-		var v = Velocity;
-		float dt = (float)delta;
-
-		// Gravity
-		if (!IsOnFloor())
-			v.Y += _gravity * dt;
-
-		// Use built-in actions to avoid InputMap issues
-		float input = Input.GetAxis("ui_left", "ui_right");
-		if (Mathf.Abs(input) > 0.01f)
-			v.X = input * MoveSpeed;
-		else
-			v.X = IsOnFloor() ? Mathf.MoveToward(v.X, 0, 2000f * dt) : v.X;
-
-		// Jump
-		if (IsOnFloor() && Input.IsActionJustPressed("ui_up"))
-			v.Y = -JumpVelocity;
-
-		Velocity = v;
 		MoveAndSlide();
-
-		// Debug prints (temporarily)
-		// GD.Print($"input={input}, onFloor={IsOnFloor()}, v=({Velocity.X:0.0},{Velocity.Y:0.0})");
 	}
+
+	// Falling state
+	public void player_falling(double delta)
+	{
+		if (!IsOnFloor())
+		{
+			Velocity += new Vector2(0, _gravity * (float)delta);
+		}
+	}
+	
+	public void player_idle(double delta)
+	{
+        if (IsOnFloor())
+		{
+			current_state = State.Idle;
+		}
+    }
 }
